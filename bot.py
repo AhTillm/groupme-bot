4c280a9b77ee3b6eefb5866d86f72c630480f91f
Joshua Norman <jmnorma@clemson.edu>
2019-11-07 23:33:13 -0500

    Changed send logic

--- bot.py
+++ bot.py
@@ -1,18 +1,12 @@
 import os
 import random
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 from flask import Flask, json, request
 import requests
 
 
-		app = Flask(__name__)
-
-def reply(message):
-	payload = {
-		'bot_id' : os.environ['BOT_ID'],
-		'text'   : message,
-	}
-	requests.post('https://api.groupme.com/v3/bots/post', json=payload)
-
+app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def groupme_callback():
@@ -28,6 +22,18 @@ def groupme_callback():
 		if userName == 'Josh Norman':
 			message = 'whats up'
 			reply(message)
+	
+	return "ok", 200
+
+def reply(message):
+	payload = {
+		'bot_id' : os.environ['BOT_ID'],
+		'text'   : message,
+	}
+	request = Request('https://api.groupme.com/v3/bots/post', urlencode(payload).encode())
+	json = urlopen(request).read().decode()
+
+
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
